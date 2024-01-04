24b6a76304f12181905f941c668d0d41b2f3b610
Arnold Atsé <arnold.arnold@gmail.com>
2024-01-04 23:33:08 +0000

    refactor: removed MapperEntitiesSetable.java

--- core/src/main/java/dev/arnoldatse/opensource/look4dev/core/mappers/MapperEntitiesSetable.java
+++
@@ -1,6 +0,0 @@
-package dev.arnoldatse.opensource.look4dev.core.mappers;
-
-public interface MapperEntitiesSetable <Entity, MatchEntity>{
-    public void setMainEntity(Entity entity);
-    public void setMatchEntity(MatchEntity matchEntity);
-}
