726d9731cf1a166c8256f227c4f348d465a1b359
Arnold Atsé <atse.arnold@gmail.com>
2024-01-27 15:03:44 +0000

    fix(profile-details-upload): added routes for upload profile picture and cv.

--- app/src/main/java/dev/arnoldatse/opensource/look4dev/app/rest/UserProfileDetailsController.java
+++ app/src/main/java/dev/arnoldatse/opensource/look4dev/app/rest/UserProfileDetailsController.java
@@ -2,13 +2,17 @@ package dev.arnoldatse.opensource.look4dev.app.rest;
 
 import dev.arnoldatse.opensource.look4dev.app.userProfileDetails.UserProfileDetailsService;
 import dev.arnoldatse.opensource.look4dev.core.entities.user.dtos.userProfileDetailsDto.PasswordUpdateRequestDto;
+import dev.arnoldatse.opensource.look4dev.core.entities.user.dtos.userProfileDetailsDto.UserProfileDetailsFileUrlResponseDto;
 import dev.arnoldatse.opensource.look4dev.core.entities.user.dtos.userProfileDetailsDto.UserProfileDetailsResponseDto;
 import dev.arnoldatse.opensource.look4dev.core.entities.user.dtos.userProfileDetailsDto.UserProfileDetailsUpdateRequestDto;
+import dev.arnoldatse.opensource.look4dev.core.fileStorage.FailedToStoreFileException;
 import dev.arnoldatse.opensource.look4dev.core.http.DefaultHttpResponse;
+import dev.arnoldatse.opensource.look4dev.core.http.defaultExceptions.FileExtensionNotSupportedException;
 import dev.arnoldatse.opensource.look4dev.core.http.defaultExceptions.NotFoundException;
 import dev.arnoldatse.opensource.look4dev.core.http.defaultExceptions.RepositoryException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/profile")
@@ -28,5 +32,15 @@ public class UserProfileDetailsController {
     @PatchMapping("/update-password")
     public DefaultHttpResponse update(@RequestBody PasswordUpdateRequestDto passwordUpdateRequestDto) throws Exception {
         return userProfileDetailsService.updatePassword(passwordUpdateRequestDto);
+    }
+
+    @PostMapping("/update-picture")
+    public UserProfileDetailsFileUrlResponseDto updatePicture(@RequestParam("picture") MultipartFile picture) throws FailedToStoreFileException, NotFoundException, FileExtensionNotSupportedException {
+        return userProfileDetailsService.updatePicture(picture);
+    }
+
+    @PostMapping("/update-cv")
+    public UserProfileDetailsFileUrlResponseDto updateCv(@RequestParam("cv") MultipartFile cv) throws FailedToStoreFileException, NotFoundException, FileExtensionNotSupportedException {
+        return userProfileDetailsService.updateCv(cv);
     }
 }
