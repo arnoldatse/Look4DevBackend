efc8983cb3239dda6b4be87a622cb2838952b042
Arnold Atsé <atse.arnold@gmail.com>
2024-01-30 00:31:01 +0000

    refactor: core renamed core fileStorageAdapters implementations.

--- core/src/main/java/dev/arnoldatse/opensource/look4dev/core/users/usecases/userProfileDetails/profilePicture/UpdateUserProfilePicture.java
+++ core/src/main/java/dev/arnoldatse/opensource/look4dev/core/users/usecases/userProfileDetails/profilePicture/UpdateUserProfilePicture.java
@@ -15,14 +15,14 @@ import dev.arnoldatse.opensource.look4dev.core.utils.RandomString;
 public class UpdateUserProfilePicture<T> {
     private final User user;
     private final UserRepository userRepository;
-    private final FileStoragePersistAndUrlGetterAdapter<T> fileStorage;
+    private final FileStoragePersistAndUrlGetterAdapter<T> fileStoragePersistWithUrlGetter;
     private final T picture;
     private final String fileExtention;
 
-    public UpdateUserProfilePicture(User user, UserRepository userRepository, FileStoragePersistAndUrlGetterAdapter<T> fileStorage, T picture, String fileExtention) {
+    public UpdateUserProfilePicture(User user, UserRepository userRepository, FileStoragePersistAndUrlGetterAdapter<T> fileStoragePersistWithUrlGetter, T picture, String fileExtention) {
         this.user = user;
         this.userRepository = userRepository;
-        this.fileStorage = fileStorage;
+        this.fileStoragePersistWithUrlGetter = fileStoragePersistWithUrlGetter;
         this.picture = picture;
         this.fileExtention = fileExtention;
     }
@@ -34,9 +34,9 @@ public class UpdateUserProfilePicture<T> {
             String fileName = generateFileName();
             String fullFileName = fileName + "." + fileExtention;
 
-            fileStorage.store(picture, fileName, fileExtention, FilesTypesDirectories.UserProfilePicture);
+            fileStoragePersistWithUrlGetter.store(picture, fileName, fileExtention, FilesTypesDirectories.UserProfilePicture);
             userRepository.updateUserPicture(user.getId(), fullFileName);
-            return new UserProfileDetailsFileUrlResponseDto(fileStorage.getUrl(FilesTypesUrlsParts.UserProfilePicture, fullFileName));
+            return new UserProfileDetailsFileUrlResponseDto(fileStoragePersistWithUrlGetter.getUrl(FilesTypesUrlsParts.UserProfilePicture, fullFileName));
         }
         throw new FileExtensionNotSupportedException();
     }
@@ -44,7 +44,7 @@ public class UpdateUserProfilePicture<T> {
     private void deleteOldProfilePicture() {
         String oldProfilePicture = user.getPicture();
         if (oldProfilePicture != null) {
-            fileStorage.delete(oldProfilePicture, FilesTypesDirectories.UserProfilePicture);
+            fileStoragePersistWithUrlGetter.delete(oldProfilePicture, FilesTypesDirectories.UserProfilePicture);
         }
     }
 
